26b289071eb891ee07115dc7301ba29df4733b73
yernur04 <43569987+yernur04@users.noreply.github.com>
2019-02-08 23:27:03 +0600

    Add files via upload

---
+++ week2/Task 3/Task 3/Task 3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Task_3
+{
+    class Program
+    {
+        static string Space(int a)
+        {
+            string sp = "    ";
+            string qp = "";
+            for(int i = 0; i < a; i++)
+            {
+                qp += sp;
+            }
+            return qp;
+        }
+        static void Pap(DirectoryInfo direct, int a)
+        {
+            FileInfo[] files = direct.GetFiles();
+            DirectoryInfo[] folders = direct.GetDirectories();
+            foreach(FileInfo file in files)
+            {
+                Console.Write(Space(a));
+                Console.WriteLine(file.Name);
+            }
+            foreach(DirectoryInfo file in folders)
+            {
+                Console.Write(Space(a));
+                Console.WriteLine(file.Name);
+                Pap(file, a + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo direct = new DirectoryInfo(@"C:\Users\Yernur\Desktop\C#");
+            Pap(direct, 0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ week2/Task 4/Task 4/Task 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = @"C:\Users\Yernur\Desktop\C#\week2\Task 4\Task 4\Text1.txt";
+            string b = @"C:\Users\Yernur\Desktop\C#\week2\Task 4\Task 4\Text2.txt";
+            StreamWriter wr = new StreamWriter(a);
+            string text = "yernur_";
+            wr.Write(text);
+            wr.Close();
+            File.Copy(a, b);
+            File.Delete(a);
+        }
+    }
+}
